bd71c7d04c4b51c6642f82c66d5f4201988f358c
Aditya Majumdar <76590375+Aditya-pro2@users.noreply.github.com>
2025-09-10 03:57:29 +0000

    LeetSyncMaster - Add Two Numbers II (Python3)

---
+++ problems/0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        s1, s2 = "", ""
+        while l1:
+            s1 += str(l1.val)
+            l1 = l1.next
+        while l2:
+            s2 += str(l2.val)
+            l2 = l2.next
+        r = str(int(s1) + int(s2))
+        curr = ListNode(0)
+        head = curr
+        for i in r:
+            curr.next = ListNode(int(i))
+            curr = curr.next
+        return head.next
