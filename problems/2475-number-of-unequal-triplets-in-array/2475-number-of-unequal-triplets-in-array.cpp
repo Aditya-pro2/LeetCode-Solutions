99b2c128b97d53218596cab588073d3157be2d86
Aditya Majumdar <76590375+Aditya-pro2@users.noreply.github.com>
2025-09-21 01:54:44 +0000

    LeetSyncMaster - Number of Unequal Triplets in Array (C++)

---
+++ problems/2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int unequalTriplets(vector<int>& nums) {
+        int trips = 0, pairs = 0, count[1001] = {};
+        for (int i = 0; i < nums.size(); i++)
+        {
+            trips += pairs - count[nums[i]] * (i - count[nums[i]]);
+            pairs += i - count[nums[i]];
+            count[nums[i]] += 1;
+        }
+        return trips;
+    }
+};
